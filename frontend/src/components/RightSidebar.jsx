0ec9921535ddab5345be79506e2ebae9222c0efc
Mitul82 <mitulsrivas@gmail.com>
2025-11-27 18:11:36 +0530

    fixed frontend syntax errors

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -11,7 +11,7 @@ function RightSidebar() {
     const [msgImages, setmsgImages] = React.useState([]);
 
     React.useEffect(() => {
-        setmsgImages(messages.filter(msg => msg.image)).map(msg => msg.image);
+        setmsgImages(messages.filter(msg => msg.image).map(msg => msg.image));
     }, [messages]);
 
     return selectedUser && (
@@ -19,7 +19,7 @@ function RightSidebar() {
             <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
                 <img src={selectedUser?.profilePic || assets.avatar_icon} alt='' className='w-20 aspect-square rounded-full'/>
                 <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
-                    {onlineUsers.includes(selectedUser._id) && <p className='w-2 h-2 rounded-full bg-green-500'></p>}
+                    {onlineUsers?.includes(selectedUser._id) && <p className='w-2 h-2 rounded-full bg-green-500'></p>}
                     {selectedUser.fullName}
                 </h1>
                 <p className='px-10 mx-auto'>{selectedUser.bio}</p>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -47,7 +47,7 @@ function Sidebar() {
                         <div className='flex flex-col leading-5'>
                             <p>{user.fullName}</p>
                             {
-                                onlineUsers.includes(user._id) ? <span className='text-green-400 text-xs'>Online</span> : <span className='text-neutral-400 text-xs'>Offline</span>
+                                onlineUsers?.includes(user._id) ? <span className='text-green-400 text-xs'>Online</span> : <span className='text-neutral-400 text-xs'>Offline</span>
                             }
                         </div>
                         {unseenMessages[user._id] > 0 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-white text-black'>{unseenMessages[user._id]}</p>}
